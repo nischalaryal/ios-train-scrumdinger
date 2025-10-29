81bb6fb0a79ef8def3e63bb18f6f4d8987ff7148
nischal_mac <aryalnischal.na@gmail.com>
2025-10-29 11:54:21 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nischal Aryal on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
